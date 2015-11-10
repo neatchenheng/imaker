b65f174a7670f0f28944d12abb4d6803b23ddfc2
Henry <neatchenheng@gmail.com>
2015-11-10 23:59:31 +0800

    ajax submit

--- xpage/src/main/java/com/i9144/xpage/action/PluginAction.java
+++ xpage/src/main/java/com/i9144/xpage/action/PluginAction.java
@@ -44,8 +44,8 @@ public class PluginAction {
 		return "/plugins/list";
 	}
 
-	@RequestMapping(value = "{id}", method = RequestMethod.POST)
-	public String  update(HttpServletResponse response, Model model, @PathVariable("id") int id,
+	@RequestMapping(value = "{id}", method = RequestMethod.POST, produces="application/json;charset=UTF-8")
+	public @ResponseBody int  update(Model model, @PathVariable("id") int id,
 			@ModelAttribute Plugin plugin) {
 		plugin.setId(id);
 		pluginService.update(plugin);
