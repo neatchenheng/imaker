0e2ebb887718fbcdc2381392c03efb4b6507376a
chenheng <chenheng@tudou.com>
2015-11-26 17:44:47 +0800

    init xint project

---
+++ xint/src/main/java/com/i9144/xapi/dao/AbstractDAO.java
@@ -0,0 +1,51 @@
+package com.i9144.xapi.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
+
+public abstract class AbstractDAO<T> {
+	@Resource(name = "sqlMapClient")
+	private SqlMapClientTemplate sqlMapClient;
+	protected String daoName;
+
+	public AbstractDAO() {
+		String name = getClass().getName();
+		String strs[] = name.split("\\.");
+		daoName = strs[strs.length - 1];
+	}
+
+	public Object add(T t) {
+		Object result = sqlMapClient.insert(daoName + ".add", t);
+		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	public T get(int id) {
+		return (T) sqlMapClient.queryForObject(daoName + ".get", id);
+	}
+
+	public int update(T t) {
+		return sqlMapClient.update(daoName + ".update", t);
+	}
+
+	public int delete(int id) {
+		return sqlMapClient.delete(daoName + ".delete", id);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<T> list() {
+		return sqlMapClient.queryForList(daoName + ".list");
+	}
+
+	public int count() {
+		return (Integer) sqlMapClient.queryForObject(daoName + ".count");
+	}
+
+	public SqlMapClientTemplate getSqlMapClient() {
+		return sqlMapClient;
+	}
+}
