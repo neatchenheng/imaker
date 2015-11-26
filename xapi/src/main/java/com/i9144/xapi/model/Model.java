af761b563f128f2923badd20425c96223f8ef783
chenheng <chenheng@tudou.com>
2015-11-26 17:20:58 +0800

    init xapi skeleton

---
+++ xapi/src/main/java/com/i9144/xapi/model/Model.java
@@ -0,0 +1,9 @@
+package com.i9144.xapi.model;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+public class Model {
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this);
+	}
+}
